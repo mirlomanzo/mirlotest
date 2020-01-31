a6f369bf50bb2dfb3cd466a70f18a2cc73693764
Mirlo Manzo <mirlomanzo@hotmail.com>
2020-01-31 10:16:26 -0600

    First commit.

---
+++ app/javascript/packs/custom.js
@@ -0,0 +1,18 @@
+window.validate = function (obj1, obj2){
+		if (+obj2.value < +obj1.value){
+			obj2.value = obj1.value;
+		}
+		
+	}
+
+window.chkallchilds = function(chk) {
+		chk1.checked=chk.checked;
+		chk2.checked=chk.checked;
+		chk3.checked=chk.checked;
+		chk4.checked=chk.checked;
+		chk5.checked=chk.checked;
+		chk6.checked=chk.checked;
+		chk7.checked=chk.checked;
+}
+
+
---
+++ public/utils.js
@@ -0,0 +1,17 @@
+function validate(obj1, obj2){
+		if (+obj2.value < +obj1.value){
+			obj2.value = obj1.value;
+		}
+		
+	}
+	function chkallchilds(chk){
+		alert("hi");
+		chk1.checked=chk.checked;
+		chk2.checked=chk.checked;
+		chk3.checked=chk.checked;
+		chk4.checked=chk.checked;
+		chk5.checked=chk.checked;
+		chk6.checked=chk.checked;
+		chk7.checked=chk.checked;
+			
+	}
